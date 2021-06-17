49527661342681ed2ab19b75edfc46f2df660010
차지훈 <jihun.cha@tisquare.com>
2021-06-17 17:55:08 +0900

    짤톡좀그만하자..

---
+++ com/huni/my_note/theory/dynamic_programming/fibonacci_sample_recursive.py
@@ -0,0 +1,52 @@
+# 일반적인 재귀
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    return fibo(x - 1) + fibo(x - 2)
+
+print(fibo(4))      # 3
+
+# 메모이제이션 기법 사용
+d = [0] * 100
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+print(fibo(99))  # 218922995834555169026
+
+
+# string 찍어 보기
+d = [0] * 100
+
+def fibo(x):
+    print('f(' + str(x) + ')', end=' ')
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+print(fibo(6))
+
+# 보텀 업 방식 (반복)
+d = [0] * 100
+
+d[1] = 1
+d[2] = 1
+n = 99
+
+for i in range(3, n + 1):
+    d[i] = d[i-1] + d[i-2]
+
+print(d[n])
+
+# 218922995834555169026
+
