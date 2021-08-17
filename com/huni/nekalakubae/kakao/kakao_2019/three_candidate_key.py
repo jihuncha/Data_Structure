384aff9fb9354f378300d56704281921ad7fa85e
차지훈 <jihun.cha@tisquare.com>
2021-08-17 17:38:47 +0900

    답적어두고 집갈준비

--- com/huni/nekalakubae/kakao/kakao_2019/three_candidate_key.py
+++ com/huni/nekalakubae/kakao/kakao_2019/three_candidate_key.py
@@ -40,6 +40,7 @@
 # 문제에 주어진 릴레이션과 같으며, 후보 키는 2개이다.
 
 ## dfs..?
+# 조합만드는거를 해결못햇음..구현이 아직도 미숙한듯
 
 import itertools
 def solution(relation):
@@ -52,6 +53,64 @@ def solution(relation):
 
 
     return answer
+
+print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
+
+from itertools import combinations
+
+def solution(relation):
+    answer = 0
+
+    #컬럼 갯수
+    temp = [i for i in range(len(relation[0]))]
+    # print(temp)
+
+    # 조합담을 list
+    combi_lists = list()
+
+    # 조합 생성 -> combination으로 1~전체 갯수만큼 숫자로 생성 (index)
+    for cnt in range(1, len(relation[0]) + 1):
+        combi_lists.append(list(combinations(temp, cnt)))
+    # print(combi_lists)
+
+    # 조합리스트를 문자열로 변경
+    combi_str_list = list()
+    for combi_list in combi_lists:
+        for i in combi_list:
+            combi_str_list.append(''.join(map(str, i)))
+
+    # print(combi_str_list)
+    # ['0', '1', '2', '3', '01', '02', '03', '12', '13', '23', '012', '013', '023', '123', '0123']
+
+    while len(combi_str_list) > 0:
+        candidate_key = list()
+        is_candidate_key = True
+
+        for r in relation:
+            key = ""
+            for j in combi_str_list[0]:
+                key += r[int(j)]
+            print(key)
+
+            if key in candidate_key:
+                is_candidate_key = False
+                break
+            else:
+                candidate_key.append(key)
+
+        if is_candidate_key is False:
+            del combi_str_list[0]
+            continue
+
+        str_list = list(combi_str_list[0])
+        print(str_list)
+
+        combi_str_list = [s for s in combi_str_list if any(str not in s for str in str_list)]
+
+        answer += 1
+
+    return answer
+
 
 print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
 
